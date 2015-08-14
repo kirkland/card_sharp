15d5db0e41e092bbca9429022c892d5a823a83e7
Robert Kaufman <kaufman@scvngr.com>
2015-08-14 16:32:15 -0400

    Zooming demo, using x and z on keyboard

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+function set_scale(new_scale) {
+  $('body').css({ transform: 'scale(' + new_scale + ')' });
+}
+
 $(function() {
+  var current_scale = 1;
+
+  $(document).keypress(function(e) {
+    var key = e.which;
+    if (key === 120) { // x
+      current_scale = current_scale * 1.05;
+      set_scale(current_scale);
+    } else if (key === 122) { // y
+      current_scale = current_scale * 0.95;
+      set_scale(current_scale);
+    }
+  });
 });
