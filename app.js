d0b88b86d6ecf1db3d2bdb737879dc2a924ff38c
Robert Kaufman <kaufman@scvngr.com>
2015-08-16 15:37:23 -0400

    Reboot debug statements

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ function percent_distance_from_main_to_cursor(cursor_x) {
 }
 
 function desired_distance_from_main_to_cursor(percent) {
-  console.log('desired distance from main to cursor', percent * main_width());
   return percent * main_width();
 }
 
@@ -46,20 +45,12 @@ function zoom(delta) {
 
 function react_to_mouse(wheel_delta, cursor_x) {
   var starting_percent_distance_from_main_to_cursor = percent_distance_from_main_to_cursor(cursor_x);
-  console.log('percent distance from main left to cursor', starting_percent_distance_from_main_to_cursor);
 
   zoom(wheel_delta);
 
-  console.log('distance fom left side to main', $('#main').position().left);
   var main_offset = main_left_offset_to_restore_percent(cursor_x, starting_percent_distance_from_main_to_cursor);
-  console.log('desired main offset', main_offset);
 
   set_translate_x(main_offset);
-
-  console.log('actual distance from main to cursor', cursor_x - $('#main').position().left);
-
-  // Should match other log
-  console.log('new percent distance from main left to cursor', percent_distance_from_main_to_cursor(cursor_x));
 }
 
 $(function() {
