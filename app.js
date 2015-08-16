ebdecf899029d1832b64d62d04728517e5ab343f
Robert Kaufman <kaufman@scvngr.com>
2015-08-16 14:25:43 -0400

    Change transformations to be on div, not body

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ debug_once = true
 current_scale = 1;
 
 function set_scale(new_scale) {
-  $('body').css({ transform: 'scale(' + new_scale + ')' });
+  $('#main').css({ transform: 'scale(' + new_scale + ')' });
 }
 
-function body_width() {
-  return $('body').width() * current_scale;
+function main_width() {
+  return $('main').width() * current_scale;
 }
 
 function percent_distance_from_left_to_cursor(event) {
-  var distance_from_left_to_cursor = event.originalEvent.screenX - $('body').position().left;
-  return distance_from_left_to_cursor / body_width();
+  var distance_from_left_to_cursor = event.originalEvent.screenX - $('#main').position().left;
+  return distance_from_left_to_cursor / main_width();
 }
 
 function desired_distance_from_left_to_cursor(percent) {
-  return percent * body_width();
+  return percent * main_width();
 }
 
-function body_left_offset_to_restore_percent(cursor_distance_from_left, percent) {
+function main_left_offset_to_restore_percent(cursor_distance_from_left, percent) {
   return  cursor_distance_from_left - desired_distance_from_left_to_cursor(percent);
 }
 
@@ -40,7 +40,7 @@ $(function() {
 
     if ( debug_once || true ) {
       console.log('cursor', e.originalEvent.screenX, e.originalEvent.screenY);
-      console.log('body', $('body').position().left, $('body').position().top);
+      console.log('main', $('#main').position().left, $('#main').position().top);
       debug_once = false;
     }
 
@@ -48,13 +48,13 @@ $(function() {
 
     zoom(delta);
 
-    // Now do a translate on body so that it gets back to the percent it was before
-    // The body width has changed
+    // Now do a translate on main so that it gets back to the percent it was before
+    // The main width has changed
     // Now we want to change... something... to make percent_distance_from_left_to_cursor to be 
     // the same as before.
-    // Well, we're not going to change the cursor position, so that leaves body's offset.
+    // Well, we're not going to change the cursor position, so that leaves main's offset.
 
-    console.log('actual body offset', $('body').position().left);
-    console.log('desired body offset', body_left_offset_to_restore_percent(e.originalEvent.screenX, starting_percent_distance_from_left_to_cursor));
+    console.log('actual main offset', $('#main').position().left);
+    console.log('desired main offset', main_left_offset_to_restore_percent(e.originalEvent.screenX, starting_percent_distance_from_left_to_cursor));
   });
 });
