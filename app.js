c788fcb93f4ce923fe14f2110ee250da9633bace
Robert Kaufman <kaufman@scvngr.com>
2015-08-17 09:14:42 -0400

    Similar spot to before

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-mainScale = 1;
+bodyScale = 1;
 translateX = 0;
 translateY = 0;
 
-function updateMainTransform() {
-  $('#main').css({ transform: 'scale(' + mainScale + ') translate(' + translateX + 'px, ' +
+function updateBodyTransform() {
+  $('body').css({ transform: 'scale(' + bodyScale + ') translate(' + translateX + 'px, ' +
     translateY + 'px)' });
 }
 
-function setMainScale(newMainScale) {
-  mainScale = newMainScale;
-  updateMainTransform();
+function setBodyScale(newBodyScale) {
+  bodyScale = newBodyScale;
+  updateBodyTransform();
+}
+
+function setBodyTranslateX(newTranslateX) {
+  translateX = newTranslateX;
+  updateBodyTransform();
 }
 
 function zoom(directionIn) {
   if ( directionIn ) {
-    setMainScale(mainScale * 1.02);
+    setBodyScale(bodyScale * 1.02);
   } else {
-    setMainScale(mainScale * 0.98);
+    setBodyScale(bodyScale * 0.98);
   }
 }
 
+function bodyWidth() {
+  return $('body').width() * bodyScale;
+}
+
+function leftDistanceFromBodyToTarget(targetX) {
+  var leftDistanceFromWindowToTarget = targetX;
+  var leftDistanceFromWindowToBody = $('body').position().left;
+  return leftDistanceFromWindowToTarget - leftDistanceFromWindowToBody;
+}
+
 function zoomAndTranslate(directionIn, targetX, targetY) {
-  console.log(directionIn, targetX, targetY);
+  var leftPercentDistanceFromBodyToTarget = leftDistanceFromBodyToTarget(targetX) / bodyWidth();
+  console.log(leftPercentDistanceFromBodyToTarget);
+
   zoom(directionIn);
+
+  var leftDesiredDistanceFromBodyToTarget = bodyWidth() * leftPercentDistanceFromBodyToTarget;
+  var leftActualDistanceFromBodyToTarget = leftDistanceFromBodyToTarget(targetX);
+  setBodyTranslateX(translateX + (leftActualDistanceFromBodyToTarget - leftDesiredDistanceFromBodyToTarget));
 }
 
 $(function() {
