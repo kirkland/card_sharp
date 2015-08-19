7b5832b179a81e8f7fc8f5f8f86851b66ddb355b
Robert Kaufman <kaufman@scvngr.com>
2015-08-19 16:38:36 -0400

    Remove trace and debugging

--- app.js
+++ app.js
@@ -48,38 +48,22 @@ function topPercentDistanceFromBodyToTarget(targetY) {
   return topDistanceFromBodyToTarget(targetY) / bodyWidth();
 }
 
-function traceClick(targetX, targetY) {
-  $('#marker').remove()
-  $('<div id="marker" style="position: fixed; left: 0; top: 0; border: 1px solid blue; height: ' + targetY + 'px; width: ' + targetX + 'px;"></div>').appendTo('html');
-}
-
 function zoomAndTranslate(directionIn, targetX, targetY) {
   var startingLeftPercentDistanceFromBodyToTarget = leftPercentDistanceFromBodyToTarget(targetX);
   var startingTopPercentDistanceFromBodyToTarget = topPercentDistanceFromBodyToTarget(targetY);
 
-//  traceClick(targetX, targetY);
-
-  console.log("BEFORE ZOOM")
-  console.log("starting percents:", startingLeftPercentDistanceFromBodyToTarget, startingTopPercentDistanceFromBodyToTarget);
-  console.log('topDistanceFromBodyToTarget', topDistanceFromBodyToTarget(targetY));
-
   zoom(directionIn);
-
-  console.log("AFTER ZOOM")
 
   var leftDesiredDistanceFromBodyToTarget = bodyWidth() * startingLeftPercentDistanceFromBodyToTarget;
   var leftActualDistanceFromBodyToTarget = leftDistanceFromBodyToTarget(targetX);
 
   var topDesiredDistanceFromBodyToTarget = bodyHeight() * startingTopPercentDistanceFromBodyToTarget;
   var topActualDistanceFromBodyToTarget = topDistanceFromBodyToTarget(targetY);
-  // somethihng wrong... cursor is not now 19+ pixels from the top
-  console.log('topActualDistanceFromBodyToTarget', topActualDistanceFromBodyToTarget);
 
   translateX = translateX + (leftActualDistanceFromBodyToTarget - leftDesiredDistanceFromBodyToTarget);
   translateY = translateY + (topActualDistanceFromBodyToTarget - topDesiredDistanceFromBodyToTarget);
   updateBodyTransform()
 
-  console.log("ending percents:", leftPercentDistanceFromBodyToTarget(targetX), topPercentDistanceFromBodyToTarget(targetY));
 }
 
 $(function() {
@@ -87,7 +71,6 @@ $(function() {
     event.preventDefault();
 
     var originalEvent = event.originalEvent;
-    console.log('originalEvent', originalEvent);
     zoomAndTranslate(originalEvent.wheelDelta > 0, originalEvent.clientX, originalEvent.clientY);
   });
 });
