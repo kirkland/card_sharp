2c59d68756132a9b41615a40ea6305a1fdd8f9a2
Robert Kaufman <kaufman@scvngr.com>
2015-08-21 15:41:51 -0400

    Cleanup

--- drag.js
+++ drag.js
@@ -1,6 +1,8 @@
 // Fix draggable issue for scaled elements
 // http://stackoverflow.com/questions/10212683/jquery-drag-resize-with-css-transform-scale
 
+// Must load zoom before drag
+
 // Necessary if zoom is not being used
 if (typeof zoom === 'undefined') {
   zoom = { scale: 1 };
@@ -13,9 +15,10 @@ if (typeof zoom === 'undefined') {
         ui.position.left = 0;
         ui.position.top = 0;
       },
+
       drag: function(event, ui) {
         var changeLeft = ui.position.left - ui.originalPosition.left;
-        var newLeft = ui.originalPosition.left + changeLeft / (( zoom.scale));
+        var newLeft = ui.originalPosition.left + changeLeft / zoom.scale;
 
         var changeTop = ui.position.top - ui.originalPosition.top;
         var newTop = ui.originalPosition.top + changeTop / zoom.scale;
