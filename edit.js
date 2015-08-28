87b4938c9e6206336cc6e88c1458926b859b4c35
Robert Kaufman <kaufman@scvngr.com>
2015-08-28 11:21:31 -0400

    Edit and drag compatibility, except that editing once disables drag forever

--- edit.js
+++ edit.js
@@ -2,8 +2,10 @@ $(function() {
   var editor = new nicEditor();
 
   $('.card').dblclick(function(event) {
+    editor.removeInstance('editor');
     $('#editor').removeAttr('id');
     $(event.currentTarget).find('.body').attr('id', 'editor');
+    $(event.currentTarget).draggable({ disabled: true });
     editor.addInstance('editor');
   });
 });
