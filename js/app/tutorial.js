b3591e76eeec1a141ed546c37a6d7dc3e4770a62
Robert Kaufman <kaufman@scvngr.com>
2015-09-11 14:25:33 -0400

    Make cards the 3x5 proportion and hold the typical number of lines of text

--- js/app/tutorial.js
+++ js/app/tutorial.js
@@ -1,15 +1,12 @@
 define(['jquery', 'app/edit'], function($, edit) {
   $(function() {
     var card = edit.addCard().css({ left: '100px', top: '100px' })
-    card.find('.title h1').html('First card');
-    card.find('.body').html('<p>Try dragging cards around</p>');
+    card.find('.body').html('<p>First card</p><p>Try dragging cards around</p>');
 
-    card = edit.addCard().css({ left: '350px', top: '100px' })
-    card.find('.title h1').html('Second card');
-    card.find('.body').html('<p>Try zooming in and out with your scroll wheel</p>');
+    card = edit.addCard().css({ left: '450px', top: '100px' })
+    card.find('.body').html('<p>Second card</p><p>Try zooming in and out with your scroll wheel</p>');
 
-    card = edit.addCard().css({ left: '350px', top: '300px' })
-    card.find('.title h1').html('Third card');
-    card.find('.body').html('<p>You can also double click a card to edit it</p>');
+    card = edit.addCard().css({ left: '450px', top: '300px' })
+    card.find('.body').html('<p>Third card</p><p>You can also double click a card to edit it</p>');
   });
 });
